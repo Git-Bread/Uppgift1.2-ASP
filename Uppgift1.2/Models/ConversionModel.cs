8fbdd38a82ffdbbe713713195a6d928c3c2dc874
Max Gagner <m.gagner@hotmail.se>
2025-01-28 15:49:15 +0100

    added calc page

---
+++ Uppgift1.2/Models/ConversionModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class ConversionModel
+    {
+        private double value;
+
+        [Required(ErrorMessage = "Value is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Value should probaly be a positive number")]
+        public double Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                ConvertedValue = Math.Round(value * 3.28084, 2);
+            }
+        }
+
+        public double ConvertedValue { get; set; }
+    }
+
+    public class ConversionModelSpeed
+    {
+        private double value;
+
+        [Required(ErrorMessage = "Value is required")]
+        [Range(0, double.MaxValue, ErrorMessage = "Value should probaly be a positive number")]
+        public double Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                ConvertedValue = Math.Round(value * 0.621371, 2);
+            }
+        }
+
+        public double ConvertedValue { get; set; }
+    }
+}
