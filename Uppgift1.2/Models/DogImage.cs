5a44e7ee2f58474fee9363bbd8eb9ccf8e2184ba
Max Gagner <m.gagner@hotmail.se>
2025-02-19 15:36:09 +0100

    changed weather json to getting random dog pic

---
+++ Uppgift1.2/Models/DogImage.cs
@@ -0,0 +1,10 @@
+﻿namespace Models
+{
+    public class DogImage
+    {
+        //the url, named message due to it being the message sent
+        public string Message { get; set; }
+        //status of the message, in case anything goes wrong
+        public string Status { get; set; }
+    }
+}
